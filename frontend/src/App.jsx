12f153abe42a9fd4dccdbe5e858530038d2f29a3
Rishavalpha9 <rishavkumarraj100@gmail.com>
2024-09-06 22:43:21 +0530

    added many jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './components/Home'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react'
+import Home from './pages/Home/Home'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Login from"./pages/Login/Login";
+import SignUp from './pages/signUp/signUp';
 
+import './index.css';
+
+const routes=(
+
+
+  <Router>
+<Routes>
+<Route path='/' exact element={<Home/>}/>
+
+<Route path='/login' exact element={<Login/>}/>
+
+<Route path='/signUp' exact element={<SignUp/>}/>
+
+
+
+</Routes>
+
+
+
+
+
+
+
+
+  </Router>
+)
+
+
+
+
+
+
+const App = () => {
   return (
-    <>
-        <Home/>
-    </>
+   
+    <div>{routes}</div>
   )
 }
 
