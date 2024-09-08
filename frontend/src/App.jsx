024e5ce5fcef24fa43c565dfb25826795f40946a
Rishavalpha9 <rishavkumarraj100@gmail.com>
2024-09-08 17:04:41 +0530

    added the code in login.jsx and some other changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,34 @@
 
 import React from 'react'
 import Home from './pages/Home/Home'
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import Login from"./pages/Login/Login";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./pages/Login/Login";
 import SignUp from './pages/signUp/signUp';
 
 import './index.css';
 
-const routes=(
+const routes = (
 
 
   <Router>
-<Routes>
-<Route path='/dashboard' exact element={<Home/>}/>
+    <Routes>
+      <Route path='/dashboard' exact element={<Home />} />
 
-<Route path='/login' exact element={<Login/>}/>
+      <Route path='/login' exact element={<Login />} />
 
-<Route path='/signUp' exact element={<SignUp/>}/>
-
-
-
-</Routes>
+      <Route path='/signUp' exact element={<SignUp />} />
 
 
 
-
-
-
+    </Routes>
 
 
   </Router>
 )
 
-
-
-
-
-
 const App = () => {
   return (
-   
+
     <div>{routes}</div>
   )
 }
